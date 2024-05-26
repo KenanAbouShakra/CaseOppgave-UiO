b8ea7787b01faafc8a553a7c0639d242eed4152a
KenanAbouShakra <89753703+Kenanshakra@users.noreply.github.com>
2024-05-26 19:12:21 +0200

    Initial commit

---
+++ src/main/java/com/example/caseoppgaveuio/CaseOppgaveUiOApplication.java
@@ -0,0 +1,13 @@
+package com.example.caseoppgaveuio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaseOppgaveUiOApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaseOppgaveUiOApplication.class, args);
+    }
+
+}
